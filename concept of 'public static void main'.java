9c406086602e438489c36e9fa235d4d0b3c92d8e
937msk <144563039+937msk@users.noreply.github.com>
2025-09-04 17:34:22 +0530

    Create  concept of 'public static void main'.java

---
+++ concept of 'public static void main'.java
@@ -0,0 +1,18 @@
+package assigm;
+
+public class Publicstaticmethod {
+	
+//public: The method is accessible from anywhere (JVM needs to access it)
+//static: No object is needed to run the method, JVM can call it directly
+//void: The method does not return any value
+//main: Entry point of the Java program
+	
+	public static void main(String[] args) {
+		
+		        System.out.println("Hello, World!");
+		    }
+		
+
+	}
+
+

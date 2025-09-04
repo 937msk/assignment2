9d87b64236fb5be0efd78e454cb86636a14a6ab0
937msk <144563039+937msk@users.noreply.github.com>
2025-09-04 17:14:15 +0530

    Update and rename README.md to  Return types and arguments.java

---
+++ Return types and arguments.java
@@ -0,0 +1,18 @@
+
+public class Returntypesandarguments {
+
+		
+		    public static int addNumbers(int a, int b) {
+		        return a + b;
+		    }
+
+		    public static void main(String[] args) {
+		        int result = addNumbers(5, 10);
+		        System.out.println("Sum of 5 and 10: " + result);
+		    }
+		
+
+
+	}
+
+
